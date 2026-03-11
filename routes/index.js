8a5566b779a8fc0531e99972f27e384da096e0ab
kameraizo <kameraizo69@gmail.com>
2026-03-11 10:29:03 +0100

    pages crudes -catways - reservation utilisteurs

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,43 @@ var express = require('express');  // import express
 var router = express.Router();     // import router
 
 const usersRouter = require('./users')    // import des routes
-const catwaysRouter = require('./catways')    // import des routes
-const reservationsRouter = require('./reservation')  // import des routes
+const catwaysRouter = require('./catways')    
+const reservationsRouter = require('./reservation')  
 const reservation = require('../models/reservation')
 const jwt = require('jsonwebtoken')
-
+const Catway = require('../models/catways')
+const Reservations = require('../models/reservation')
+const Users = require('../models/user')
 
 router.use('/users', usersRouter)    // use des routes
-router.use('/catways', catwaysRouter)    // use des routes
-router.use('/catways/:catwayId/reservations', (req, res, next) => {      // use des routes
+router.use('/catways', catwaysRouter)    
+router.use('/catways/:catwayId/reservations', (req, res, next) => {    
   req.catwayId = req.params.catwayId      // ajout de l'id de la catway
   next()                                  // laisse passer
 }, reservationsRouter)                    // use des routes
 
-/* GET home page. */
+router.get('/users-page', async function (req, res) {   // route de la page des utilisateurs
+  const token = req.session.token
+  if (!token) return res.redirect('/')
+  const users = await Users.find()
+  res.render('users', { users: users })
+})
+
+router.get('/reservations-page', async function (req, res) {   // route de la page des reservations
+  const token = req.session.token
+  if (!token) return res.redirect('/')
+  const reservations = await Reservations.find()
+  res.render('reservations', { reservations: reservations })
+})
+
+router.get('/catways-page', async function(req, res) {   // route de la page des catways
+  const token =req.session.token
+  if (!token) return res.redirect('/')
+    const catways = await Catway.find()
+  res.render('catways', { catways: catways })
+})
+
+
 router.get('/', function(req, res, next) {    // route de la page d'accueil
   res.render('index', { title: 'Port Russell' });    // render de la page
 });
@@ -40,5 +63,6 @@ router.get('/dashboard', async function(req, res){
     reservations: reservations
   })
 })
+
 // export
 module.exports = router;
